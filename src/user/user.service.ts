4734d9dc6855e2bc697e66fb6584ff8efb47652b
Romain Fréchet <hikarirom@gmail.com>
2020-08-14 12:26:29 +0200

    added InsertChoice function

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,12 +3,15 @@ import {InjectRepository} from "@nestjs/typeorm";
 import {User} from "./user.entity";
 import {Repository} from "typeorm";
 import {CreateUserDto} from "./create-user.dto";
+import {ChoiceService} from "../choice/choice.service";
+
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User)
-        private usersRepository: Repository<User>
+        private usersRepository: Repository<User>,
+        private choiceService: ChoiceService
     )
     {}
 
@@ -16,7 +19,21 @@ export class UserService {
         return this.usersRepository.find();
     }
     insertUser(createInformations: CreateUserDto): any {
-        let userReceived = new User(createInformations.email,createInformations.pseudo,new Date(createInformations.birthDate));
+        const userReceived = new User(createInformations.email,createInformations.pseudo,new Date(createInformations.birthDate));
         return this.usersRepository.insert(userReceived);
+    }
+    insertChoice(): any {
+        const omdb = new (require('omdbapi'))('6478392d');
+        const userId = 1;
+        const choice = omdb.search({
+            search: 'sonic'
+        }).then(function(result) {
+            const selected = result[0];
+            return {title: selected["title"], imdbid: selected['imdbid'], poster: selected["poster"] };
+        }).catch(function(error){
+            return error;
+        });
+        return this.choiceService.insertChoice(userId, choice);
+        // return choice;
     }
 }
