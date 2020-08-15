42c1a6f31402f5fa0c1e2a7dc746bcd8d37e2cb1
Romain Fréchet <hikarirom@gmail.com>
2020-08-15 22:22:08 +0200

    Add Usage of @nestjs/config for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserModule} from "./user/user.module";
 import {ChoiceModule} from "./choice/choice.module";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot(),UserModule,ChoiceModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        "entities": ["dist/**/*.entity{.ts,.js}"],
+        "synchronize": true,
+      }),
+      inject: [ConfigService],
+      }
+  ),UserModule,ChoiceModule],
   controllers: [],
   providers: [AppService],
 })
