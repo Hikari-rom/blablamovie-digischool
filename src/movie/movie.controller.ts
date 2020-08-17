9fdfca67f1043a192611db9f6debcaeabb56b824
Romain Fréchet <hikarirom@gmail.com>
2020-08-17 16:44:12 +0200

    Add movie entity & add routes

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { MovieService } from './movie.service';
+import { ConfigService } from '@nestjs/config';
+import { Movie } from './movie.entity';
 
 @Controller('movie')
 export class MovieController {
-  constructor(private movieService: MovieService) {}
+  constructor(private movieService: MovieService, private configService: ConfigService) {}
 
   @Get()
-  getBestFilmWeek(): string {
-    return 'truc';
+  getBestFilmWeek(): Promise<Movie[]> {
+    return this.movieService.getBestMovie();
+  }
+
+  @Get(':id')
+  getMovie(id: string): any {
+    return this.movieService.getMovie(id);
   }
 }
--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MovieController } from './movie.controller';
 import { ChoiceModule } from '../choice/choice.module';
+import { MovieService } from './movie.service';
 
 @Module({
   imports: [ChoiceModule],
   controllers: [MovieController],
-  providers: [],
+  providers: [MovieService],
 })
 export class MovieModule {}
